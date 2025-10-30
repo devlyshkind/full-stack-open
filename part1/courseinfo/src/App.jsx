8c065ec053ec35955be6808f1a73ce9b7a036b3a
Joaquín <29309148-devlysh-kind@users.noreply.gitlab.com>
2025-10-30 15:22:52 -0300

    refactor: compose <App /> from new components
    Create <Header />, <Content />, and <Total />.
    
    Refactor app as a composition of the aforementioned components.
    
    Part: 1
    Exercise: 1.1

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,25 +1,58 @@
+// to represent all parts composing the course
+const data = {
+  // to represent a part of the course
+  part1: {
+    name: "Fundamentals of React",
+    exercises: 10,
+  },
+  part2: {
+    name: "Using props to pass data",
+    exercises: 7,
+  },
+  part3: {
+    name: "State of a component",
+    exercises: 14,
+  },
+};
+
+// produces the header of the app
+const Header = (props) => <h1>{props.name}</h1>;
+
+// produces the content of the app
+const Content = (props) => {
+  const { part1, part2, part3 } = props.data;
+
+  return (
+    <>
+      <p>
+        {part1.name} {part1.exercises}
+      </p>
+      <p>
+        {part2.name} {part2.exercises}
+      </p>
+      <p>
+        {part3.name} {part3.exercises}
+      </p>
+    </>
+  );
+};
+
+const Total = (props) => {
+  // an iterator representing all parts composing the course
+  const parts = Object.values(props.data);
+  const total = parts.reduce((acc, part) => part.exercises + acc, 0);
+
+  return <p>Number of exercises {total}</p>;
+};
+
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = "Fundamentals of React";
-  const exercises1 = 10;
-  const part2 = "Using props to pass data";
-  const exercises2 = 7;
-  const part3 = "State of a component";
-  const exercises3 = 14;
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <Header name={course} />
+      <Content data={data} />
+      <Total data={data} />
     </div>
   );
 };
