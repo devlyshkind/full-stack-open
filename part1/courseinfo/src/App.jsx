6edb6059d8c966bb5acbd58c485cab0cd511a892
Joaquín <29309148-devlysh-kind@users.noreply.gitlab.com>
2025-10-30 16:05:41 -0300

    refactor: compose <Content /> out of <Part />
    Replace wrapper fragment for div.
    
    Create <Part /> and compose <Content /> of those.
    
    Part: 1
    Exercise: 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -18,22 +18,22 @@ const data = {
 // produces the header of the app
 const Header = (props) => <h1>{props.name}</h1>;
 
+const Part = (props) => (
+  <p>
+    {props.name} {props.exercises}
+  </p>
+);
+
 // produces the content of the app
 const Content = (props) => {
   const { part1, part2, part3 } = props.data;
 
   return (
-    <>
-      <p>
-        {part1.name} {part1.exercises}
-      </p>
-      <p>
-        {part2.name} {part2.exercises}
-      </p>
-      <p>
-        {part3.name} {part3.exercises}
-      </p>
-    </>
+    <div>
+      <Part name={part1.name} exercises={part1.exercises} />
+      <Part name={part2.name} exercises={part2.exercises} />
+      <Part name={part3.name} exercises={part3.exercises} />
+    </div>
   );
 };
 
