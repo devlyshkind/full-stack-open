cae0da11fa05624a082033c392ebed13b3996438
Joaquín <29309148-devlysh-kind@users.noreply.gitlab.com>
2025-11-11 14:54:58 -0300

    feat: collect customer feedback inside <main>
    Part: 1
    Exercise: 1.6

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,3 +1,35 @@
-const App = () => <main></main>; // stub
+import { useState } from "react";
+
+const Title = ({ name }) => <h1>{name}</h1>;
+
+const Button = ({ name, onClick }) => <button onClick={onClick}>{name}</button>;
+
+const Display = ({ name, value }) => <p>{`${name} ${value}`}</p>;
+
+const App = () => {
+  const [countGood, setCountGood] = useState(0);
+  const [countNeutral, setCountNeutral] = useState(0);
+  const [countBad, setCountBad] = useState(0);
+
+  const handleGoodButton = () => setCountGood((s) => s + 1);
+  const handleNeutralButton = () => setCountNeutral((s) => s + 1);
+  const handleBadButton = () => setCountBad((s) => s + 1);
+
+  return (
+    <main>
+      <Title name="give feedback" />
+
+      <Button name="good" onClick={handleGoodButton} />
+      <Button name="neutral" onClick={handleNeutralButton} />
+      <Button name="bad" onClick={handleBadButton} />
+
+      <Title name="statistics" />
+
+      <Display name="good" value={countGood} />
+      <Display name="neutral" value={countNeutral} />
+      <Display name="bad" value={countBad} />
+    </main>
+  );
+};
 
 export default App;
